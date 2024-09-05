6068dda8f71b6b11b8534bccc0c2ae9f06f15dc2
Cerciber <cerciber@hotmail.com>
2024-09-05 18:43:56 -0500

    chore(test): Remove unused root module config

--- src/modules/api/api.module.ts
+++ src/modules/api/api.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AccountsModule } from '@src/modules/api/modules/accounts/accounts.module';
-import { ApiController } from './api.controller';
 import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [AuthModule, AccountsModule],
-  controllers: [ApiController],
+  controllers: [],
 })
 export class ApiModule {}
--- src/statics/messages/messages.ts
+++ src/statics/messages/messages.ts
@@ -1,9 +1,4 @@
 export const messages = {
-  root: {
-    success: 'Hello was successfull!',
-    hello:
-      'Hello! You’re looking at a template for a NestJS 3-layer architecture, created by @cerciber.',
-  },
   default: {
     noFound: 'Oops! the path you’re looking for was not found.',
     unhandledError: 'Oops! An unexpected error occurred.',
